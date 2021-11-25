2373c81d97a8900c36976ac45e34049ccdd520cb
verenasworld <verena.felizidas@gmail.com>
2021-11-25 12:59:41 +0100

    add a function to pop up on image cklick

--- main.script.js
+++ main.script.js
@@ -1,3 +1,5 @@
+
+//Home und Galleryseite navigation , mobile burger on click event öfnet sich erst das Menü
 const toggleButton = document.getElementById('toggle');
 const navlist = document.getElementById('navlist');
 
@@ -6,6 +8,8 @@ toggleButton.addEventListener('click', () => {
     navlist.classList.toggle('active')
 })
 
+
+//Gallery Seite :  wenn ich auf das Bild klicke soll ein popup erscheinen über die webseite mit Mehr text und Bild
 
 let popup = document.getElementById("myModal");
 let img = document.getElementById("popup1");
