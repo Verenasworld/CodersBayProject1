438ef25d44365c2edbc74a8e4d243838dde96127
verenasworld <verena.felizidas@gmail.com>
2021-11-25 12:57:29 +0100

    add a function to pop up on image cklick

--- main.script.js
+++ main.script.js
@@ -1,8 +1,22 @@
 const toggleButton = document.getElementById('toggle');
 const navlist = document.getElementById('navlist');
-const linksInMenu = document.getElementById('skillLinks')
+
 
 toggleButton.addEventListener('click', () => {
     navlist.classList.toggle('active')
 })
 
+
+let popup = document.getElementById("myModal");
+let img = document.getElementById("popup1");
+
+img.onclick = function () {
+    popup.style.display = "block";
+}
+window.onclick = function (event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
+
+let popup2 = document.getElementById("myModal1");
