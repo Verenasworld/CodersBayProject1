fdc38bb287279f6ca3c5745be0078414c76a4692
verenasworld <verena.felizidas@gmail.com>
2021-11-28 13:46:57 +0100

    add function to get the current date and connect the html hands and set the current time to it

---
+++ javascript/clock.js
@@ -0,0 +1,23 @@
+//wir wollen alle 1000 sekunden einen Interval setzten
+
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+function setClock(){
+    const currentDate = new Date();
+    // console.log(currentDate)
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes())/ 60;
+    const hoursRatio = (minutesRatio * currentDate.getHours()) / 60;
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio){
+    element.style.setProperty("--rotation", rotationRatio *360);
+}
+
