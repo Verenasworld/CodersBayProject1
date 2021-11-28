1cb388f2fa7dd6f4729137c6f00cace26b4e4d5e
verenasworld <verena.felizidas@gmail.com>
2021-11-28 14:51:17 +0100

    fix time bug -typo

--- javascript/clock.js
+++ javascript/clock.js
@@ -1,5 +1,3 @@
-//wir wollen alle 1000 sekunden einen Interval setzten
-
 setInterval(setClock, 1000);
 
 const hourHand = document.querySelector('[data-hour-hand]');
@@ -11,7 +9,7 @@ function setClock(){
     // console.log(currentDate)
     const secondsRatio = currentDate.getSeconds() / 60;
     const minutesRatio = (secondsRatio + currentDate.getMinutes())/ 60;
-    const hoursRatio = (minutesRatio * currentDate.getHours()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
     setRotation(secondHand, secondsRatio);
     setRotation(minuteHand, minutesRatio);
     setRotation(hourHand, hoursRatio);
