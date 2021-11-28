9e47107707d55ed54b5c828632fe8d7730299d93
verenasworld <verena.felizidas@gmail.com>
2021-11-28 12:03:31 +0100

    add keyframes and js function

--- main.script.js
+++ main.script.js
@@ -1,5 +1,5 @@
 
-//Home und Galleryseite navigation , mobile burger on click event öfnet sich erst das Menü
+//Home und Galleryseite navigation , mobile burger on click event klappt sich die navigation auf
 const toggleButton = document.getElementById('toggle');
 const navlist = document.getElementById('navlist');
 
@@ -8,14 +8,24 @@ toggleButton.addEventListener('click', () => {
     navlist.classList.toggle('active')
 })
 
+// hier ensteht eine funktion für das Formular beim cklick auf senden soll eine Anzeige nur einmal vorerst erscheinen das es funktioniert hat in späterer Folge / eine if else
+function infobox(){
+    let submitbutton = document.getElementById("btn-submit").innerHTML= "deine nachricht wurde erfolgreich gesendet ";
 
+}
+
+
+
+
+//test
 //Gallery Seite :  wenn ich auf das Bild klicke soll ein popup erscheinen über die webseite mit Mehr text und Bild
 
 let popup = document.getElementById("myModal");
 let img = document.getElementById("popup1");
 
 img.onclick = function () {
-    popup.style.display = "block";
+     popup.style.display = "block";
+
 }
 window.onclick = function (event) {
     if (event.target == popup) {
@@ -23,4 +33,3 @@ window.onclick = function (event) {
     }
 }
 
-let popup2 = document.getElementById("myModal1");
